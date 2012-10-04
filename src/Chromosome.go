f9695ab453bcf5cfd75beadfcc14c8bcd31aab23
iamrekcah <mcampos@mail.bradley.edu>
2012-10-03 20:18:29 -0500

    fix: Cleaned up the function names, and separated logic into separate files
    In Go Lang, it seems that a package's functions should (or must) begin with a capital letter. I made this change, and also slightly changed the name to better describe the function. I also separated the two logic parts (Chromosome, and Population) into separate source files. This should improve readability and maintainability.

---
+++ src/Chromosome.go
@@ -0,0 +1,66 @@
+package hwga
+
+import "math/rand"
+
+type Chromosome struct {
+	code  [13]byte // genetic code
+	score     int  // fitness score
+}
+
+func (c *Chromosome) Initialize() {
+	for i := range c.code {
+		c.code[i] = byte(rand.Intn(90) + 32)
+	}
+
+	c.CalculateCost()
+}
+
+func (c *Chromosome) CalculateCost() {
+	c.score = 0
+
+	for i := range c.code {
+		c.score += c.Evaluate(i)
+	}
+}
+
+func (a *Chromosome) Mate(b *Chromosome) *Chromosome {
+	c := new(Chromosome)
+
+	for i := 0; i < 13; i++ {
+		if a.Evaluate(i) < b.Evaluate(i) {
+			c.code[i] = a.code[i]
+		} else {
+			c.code[i] = b.code[i]
+		}
+	}
+
+	return c
+}
+
+func (c *Chromosome) Evaluate(i int) int {
+	var x int
+
+	switch i {
+	case 0:  x = 72  // H
+        case 1:  x = 101 // e
+        case 2:  x = 108 // l
+        case 3:  x = 108 // l
+        case 4:  x = 111 // o
+        case 5:  x = 44  // ,
+        case 6:  x = 32  // <space>
+        case 7:  x = 87  // W
+        case 8:  x = 111 // o
+        case 9:  x = 114 // r
+        case 10: x = 108 // l
+        case 11: x = 100 // d
+        case 12: x = 33  // !
+	}
+
+	eval_val := x - int(c.code[i])
+
+	if eval_val < 0 {
+		eval_val = -eval_val
+	}
+
+	return eval_val
+}
---
+++ src/Population.go
@@ -0,0 +1,78 @@
+package hwga
+
+import "math/rand"
+
+type Population []*Chromosome;
+
+func (p Population) Populate() {
+	for i := range p {
+		p[i] = new(Chromosome)
+		p[i].Initialize()
+	}
+}
+
+func (p Population) Crossover() Population {
+	var h,j int
+	np := make(Population, len(p))
+	np.Populate()
+
+	for i := range p {
+		if i == 0 {
+			h = len(p)-1
+			j = 1
+		} else if i == len(p)-1 {
+			h = i-1
+			j = 0
+		} else {
+			h = i-1
+			j = i+1
+		}
+				
+		if p[h].score > p[j].score {
+			np[i] = p[i].Mate(p[j])
+		} else {
+			np[i] = p[i].Mate(p[h])
+		}
+	}
+
+	return np
+}
+
+func (p Population) Mutate() {
+	for i := range p {
+		pos := rand.Intn(13)
+		val := byte(rand.Intn(90) + 32)
+		p[i].code[pos] = val
+	}
+}
+
+func (p Population) Cost() {
+	for i := range p {
+		p[i].CalculateCost()
+	}
+}
+
+func (p Population) Best() *Chromosome {
+	var c *Chromosome = p[0]
+
+	for i := range p {
+		if p[i].score < c.score {
+			c = p[i]
+		}
+	}
+
+	return c
+}
+
+func (p Population) IsSolved() bool {
+	var retval bool = false
+
+	for i := range p {
+		if p[i].score == 0 {
+			retval = true
+			break
+		}
+	}
+
+	return retval	
+}
