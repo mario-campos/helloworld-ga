7dfc6179291f50da239661200781a388bc722a77
iamrekcah <mcampos@mail.bradley.edu>
2012-10-06 18:28:08 -0500

    new: added example

---
+++ example/example.go
@@ -0,0 +1,26 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+package main
+
+import (
+	"fmt"
+	"hwga"
+	"time"
+	"math/rand"
+)
+
+func main() {
+	rand.Seed(time.Now().Unix())
+	
+	p := make(hwga.Population, 1000)
+	p.Populate()
+
+	for gen := 1; !p.IsSolved(); gen++ {
+		p = p.Crossover()
+		p.Mutate()
+		p.Cost()
+		best := p.Best()
+		fmt.Printf("[ %d ]\t%s\t(%d)\n", gen, best.Code, best.Score)
+	}
+}
